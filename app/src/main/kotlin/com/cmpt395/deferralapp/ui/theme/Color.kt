9c685b102f139e2536c7c257209fda1757413653
MathPRG <gabriel.w.young@outlook.com>
2025-12-02 18:50:46 -0700

    Properly set up dark and light mode themes with new colors.

--- app/src/main/kotlin/com/cmpt395/deferralapp/ui/theme/Color.kt
+++ app/src/main/kotlin/com/cmpt395/deferralapp/ui/theme/Color.kt
@@ -2,16 +2,19 @@ package com.cmpt395.deferralapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 val Maroon = Color(0xFF800000)
 val Cream = Color(0xFFFFF5E6)
+
+val theme_light_bg = Color(0xFFffffff)
+val theme_light_onBackground = Color(0xFF000000)
+val theme_light_surface = Color(0xFFced4da)
+val theme_light_onSurface = Color(0xFF4F4F4F)
+
+val theme_dark_bg = Color(0xFF000000)
+val theme_dark_onBackground = Color(0xFFffffff)
+val theme_dark_surface = Color(0xFF2e3032)
+val theme_dark_onSurface = Color(0xFFC8C8C8)
+
 val CardGrey = Color(0xFFF2F2F2)
 val TextDark = Color(0xFF4F4F4F)
 val TextBlack = Color(0xFF000000)
